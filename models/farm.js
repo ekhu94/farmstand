6a19b07032bdb355c6a053de1fba041ec99cc216
Erik Huang <ekhu94@gmail.com>
2021-05-03 11:17:35 -0700

    adds mongoose middleware in farm model to delete products associated with it

--- models/farm.js
+++ models/farm.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const Product = require('./product');
+
 const farmSchema = new Schema({
   name: {
     type: String,
@@ -15,6 +17,12 @@ const farmSchema = new Schema({
     required: [true, 'Email required.'],
   },
   products: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+});
+
+farmSchema.post('findOneAndDelete', async function (farm) {
+  if (farm.products.length) {
+    await Product.deleteMany({ _id: { $in: farm.products } });
+  }
 });
 
 module.exports = mongoose.model('Farm', farmSchema);
